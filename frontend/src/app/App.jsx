6f2b65e85b4cc7b2f72488ecef4c4856618410f0
rohit kuwarbi <rohitdeppi38@gmail.com>
2026-09-09 22:07:28 +0530

    feat/awareness

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useRef,useMemo } from 'react'
+import { useState, useRef, useMemo } from 'react'
 import './App.css'
 
 
@@ -12,72 +12,88 @@ import { SocketIOProvider } from 'y-socket.io';
 
 function App() {
   const editorRef = useRef(null);
-  const [username,setUsername] = useState('');
+  const [username, setUsername] = useState(() => {
+    return new URLSearchParams(window.location.search).get('username') || '';
+  });
 
-  const ydoc = useMemo(()=> new Y.Doc,[]);
-  const yText = useMemo(()=> ydoc.getText("monaco"),[ydoc]);
+  const [users, setUsers] = useState([]);
+
+  const ydoc = useMemo(() => new Y.Doc, []);
+  const yText = useMemo(() => ydoc.getText("monaco"), [ydoc]);
 
   const handleMount = (editor) => {
-  editorRef.current = editor;
+    editorRef.current = editor;
 
-  const provider = new SocketIOProvider(
-    "http://localhost:3000",
-    "monaco",
-    ydoc,
-    {
-      autoConnect: true,
+  };
+
+  const handleJoin = (e) => {
+    e.preventDefault();
+
+    setUsername(e.target.username.value);
+    window.history.pushState({}, '', `?username=${e.target.username.value}`);
+  };
+
+  useEffect(()=>{
+      if(username && editorRef.current) {
+        const provider = new SocketIOProvider('http://localhost:3000', 'monaco-demo', ydoc,{
+          autoConnect : true,
+        });
+
+        provider.awareness.setLocalStateField('user',{username});
+
+        provider.awareness.on('change',()=>{
+          const states = Array.from(provider.awareness.getStates().values());
+          setUsers(states.map(state=>state.user).filter(user=>Boolean(user.username)))
+        })
+
+        const monacoBinding = new MonacoBinding(
+          yText,
+          editorRef.current.getModel(),
+          new Set([editorRef.current]),
+          provider.awareness
+        );
+
+      }
+
     }
-  );
+  ,[
+    editorRef.current,
+    username,
+  ]);
+    if (!username) {
+      return (
+        <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
+          <form className='h-screen w-full flex justify-center items-center' onSubmit={handleJoin}>
+            <input placeholder="Enter your username..."
+              className='bg-gray-800 p-2 rounded-md outline-none text-black'
+              name='username'
+            />
 
-  const monacoBinding = new MonacoBinding(
-    yText,
-    editorRef.current.getModel(),
-    new Set([editorRef.current]),
-    provider.awareness
-  );
-};
+            <button className='p-2 rounded-md bg-amber-50 text-black ml-2'
+              type='submit'
+            >Join</button>
+          </form>
+        </main>
+      )
+    }
 
-const handleJoin = () => {
-  const input = document.querySelector('input');
-  if(input.value.trim() !== ''){
-    setUsername(input.value.trim());
-  }
-}
+    return (
+      <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
+        <aside className='h-full w-[40%] rounded-md overflow-hidden bg-amber-50'>
 
-if(!username){
-  return (
-    <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
-      <div className='h-screen w-full flex justify-center items-center'>
-        <input placeholder="Enter your username..." 
-        className='p-2 rounded-md outline-none text-black'
-        
-        />
+        </aside>
 
-        <button className='p-2 rounded-md bg-amber-50 text-black ml-2'
-        onClick={handleJoin}
-        >Join</button>
-      </div>
-    </main>
-  )
-}
-
-  return (
-    <main className='h-screen w-full bg-gray-950 flex gap-4 p-4'>
-      <aside className='h-full w-[40%] rounded-md overflow-hidden bg-amber-50'>
-
-      </aside>
-
-      <section className='hfull w-[60%] bg-neutral-800 rounded-lg overflow-hidden'>
-          <Editor 
+        <section className='hfull w-[60%] bg-neutral-800 rounded-lg overflow-hidden'>
+          <Editor
             height="100%"
             defaultLanguage='javascript'
             defaultValue='// some comment'
             theme='vs-dark'
             onMount={handleMount}
           />
-      </section>
-    </main>
-  )
-}
+        </section>
+      </main>
+    )
+  }
 
 export default App
